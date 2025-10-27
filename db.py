be972e36f266e5c7b287852cda8415ba9e8e9dd5
1711vishnuap <153195786+1711vishnuap@users.noreply.github.com>
2025-10-28 00:04:08 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,8 +3,10 @@ import mysql.connector
 def get_connection():
     connection = mysql.connector.connect(
         host="fixy-db-vishnuap1711-eafd.j.aivencloud.com",
+        port="21316",
         user="avnadmin",
         password="AVNS_ZNXoQVjW1IsdYW2bIpW",
         database="FixyDB"
     )
     return connection
+
